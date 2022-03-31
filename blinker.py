7f67604fe408a09b605ccf4882ff5a765c728294
Alxpad <83509222+Alxpad@users.noreply.github.com>
2022-03-31 19:30:44 +0200

    Añadidos archivos vía subida de archivos

---
+++ blinker.py
@@ -0,0 +1,51 @@
+import threading
+from time import sleep
+
+# Blinker position (pos attribute)
+BLINKER_FRONT = 1
+BLINKER_REAR = 2
+
+class Blinker(threading.Thread):
+    def __init__(self, pos):
+        threading.Thread.__init__(self)
+
+        self.marcador = False
+        self.pos=pos  
+        self.blinking=False
+        self.deactivate()
+
+    def activate(self):
+        self.activated=True
+
+    def deactivate(self):
+        self.activated=False
+
+    def blink(self):
+        self.blinking=not self.blinking
+        if not self.blinking:
+            self.deactivate()
+        else:
+            self.activate()
+
+    def get_activated(self):
+        return self.activated
+
+
+    def change(self):
+        self.activated = not self.activated
+
+    def __str__(self):
+        if self.activated:
+            status = 'A'
+        else:
+            status = 'D'
+        return status
+
+    def run(self):
+        while True:
+            while self.marcador:
+                sleep(0.5)
+                self.blink()
+            
+            if not self.marcador:
+                self.deactivate()
